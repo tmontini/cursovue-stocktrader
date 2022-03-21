0d380f77a73bee31f92b41fadf6a527ca6099ef1
Tiago Ulisses Montini <tmontini@gmail.com>
2022-03-21 00:21:41 -0300

    feat: add stocks cards

---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Ação (Portfolio)</h1>
+</template>
+
+<script>
+export default {
+  name: "Stock"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="headline">
+        <strong>Nome <smal>(Preço: preço</smal></strong>
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field label="Quantidade" type="number" />
+        <v-btn class="green darken-3 white--text">Comprar</v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: "Stock"
+}
+</script>
+
+<style scoped>
+
+</style>
